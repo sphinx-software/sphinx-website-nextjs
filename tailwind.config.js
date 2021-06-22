a597805a5fac067791697695e63cc415652890f3
ducviet1696 <phamviet008@gmail.com>
2021-06-22 15:32:18 +0700

    Implemented custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './ui/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          150: '#BDC7CD',
+          250: '#BDC7CD',
+          350: '#505258',
+          850: '#0E1C1C'
+        },
+        violet: {
+          550: '#3C2649'
+        }
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
