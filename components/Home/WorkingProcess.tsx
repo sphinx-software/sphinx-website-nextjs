04a8a6dba8b80282d693a01a67ce743603276dda
longlh1997 <longlh@sphinx-software.com>
2021-06-24 11:19:19 +0700

    Add responsive for working progress

--- components/Home/WorkingProcess.tsx
+++ components/Home/WorkingProcess.tsx
@@ -18,11 +18,11 @@ export default function WorkingProcess() : JSX.Element {
                     <div className={'flex justify-center md:justify-start'}>
                         <p className={styles.workingContentTitle}>Requirement&apos;s analysis</p>
                     </div>
-                    <div className="flex w-full h-auto pb-250">
+                    <div className="flex w-full h-auto pb-28 lg:pb-250">
                         <div className={styles.workingContentLine}/>
-                        <div className={"flex flex-col justify-star w-full md:w-1/2"}>
-                            <div className={'flex justify-start items-end w-full flex-col'} style={{width: 536, height: 536, minWidth: 536}}>
-                                <div className='absolute z-10 rounded-full mr-4'>
+                        <div className={"flex flex-col justify-start items-end w-full md:w-1/2"}>
+                            <div className={'flex justify-start items-end w-full flex-col'}>
+                                <div className={'absolute z-10 rounded-full mr-4'}>
                                     <Image
                                         src={'/circle.svg'}
                                         height={536}
@@ -38,12 +38,12 @@ export default function WorkingProcess() : JSX.Element {
                                         alt={'deployment'}
                                     />
                                 </div>
+                                <p className={styles.workingContentImageDescription}>
+                                    After receiving inquiries from customer, the Sphinx team will indicate the project scope
+                                    and
+                                    create a plan to gain engagement with customers by using tools
+                                </p>
                             </div>
-                            <p className={styles.workingContentImageDescription}>
-                                After receiving inquiries from customer, the Sphinx team will indicate the project scope
-                                and
-                                create a plan to gain engagement with customers by using tools
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -51,10 +51,10 @@ export default function WorkingProcess() : JSX.Element {
                     <div className={'flex justify-center md:justify-start'}>
                         <p className={styles.workingContentTitle}>Design</p>
                     </div>
-                    <div className="flex w-full h-auto pb-250">
+                    <div className="flex w-full h-auto pb-28 lg:pb-250">
                         <div className={styles.workingContentLine}/>
-                        <div className={"flex flex-col justify-star w-full md:w-1/2"}>
-                            <div className={'flex justify-start items-end w-full flex-col'} style={{width: 536, height: 536, minWidth: 536}}>
+                        <div className={"flex flex-col justify-star items-end  w-full md:w-1/2"}>
+                            <div className={'flex justify-start items-center lg:items-end w-full flex-col'}>
                                 <div className='absolute z-10 rounded-full mr-4'>
                                     <Image
                                         src={'/circle.svg'}
@@ -71,14 +71,14 @@ export default function WorkingProcess() : JSX.Element {
                                         alt={'deployment'}
                                     />
                                 </div>
+                                <p className={styles.workingContentImageDescription}>
+                                    The process divides into two-stage: Internal Design and External Design. These stages
+                                    help
+                                    determine that the software meets thespecifications&apos; goals and improves the product
+                                    in
+                                    the future.
+                                </p>
                             </div>
-                            <p className={styles.workingContentImageDescription}>
-                                The process divides into two-stage: Internal Design and External Design. These stages
-                                help
-                                determine that the software meets thespecifications&apos; goals and improves the product
-                                in
-                                the future.
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -86,10 +86,10 @@ export default function WorkingProcess() : JSX.Element {
                     <div className={'flex justify-center md:justify-start'}>
                         <p className={styles.workingContentTitle}>Development</p>
                     </div>
-                    <div className="flex w-full h-auto pb-250">
+                    <div className="flex w-full h-auto pb-28 lg:pb-250">
                         <div className={styles.workingContentLine}/>
-                        <div className={"flex flex-col justify-star w-full md:w-1/2"}>
-                            <div className={'flex justify-start items-end w-full flex-col'} style={{width: 536, height: 536, minWidth: 536}}>
+                        <div className={"flex flex-col justify-star items-end w-full md:w-1/2"}>
+                            <div className={'flex justify-start items-end w-full flex-col'}>
                                 <div className='absolute z-10 rounded-full mr-4'>
                                     <Image
                                         src={'/circle.svg'}
@@ -106,11 +106,11 @@ export default function WorkingProcess() : JSX.Element {
                                         alt={'deployment'}
                                     />
                                 </div>
+                                <p className={styles.workingContentImageDescription}>
+                                    Sphinx engineer team write code and convert the design documentation into the existing
+                                    software within the software development process.
+                                </p>
                             </div>
-                            <p className={styles.workingContentImageDescription}>
-                                Sphinx engineer team write code and convert the design documentation into the existing
-                                software within the software development process.
-                            </p>
                         </div>
                     </div>
                 </div>
@@ -118,10 +118,10 @@ export default function WorkingProcess() : JSX.Element {
                     <div className={'flex justify-center md:justify-start'}>
                         <p className={styles.workingContentTitle}>Deployment</p>
                     </div>
-                    <div className="flex w-full h-auto pb-250">
+                    <div className="flex w-full h-auto pb-28 lg:pb-250">
                         <div className={styles.workingContentLine}/>
-                        <div className={"flex flex-col justify-star w-full md:w-1/2"}>
-                            <div className={'flex justify-start items-end w-full flex-col'} style={{width: 536, height: 536, minWidth: 536}}>
+                        <div className={"flex flex-col justify-star items-end w-full md:w-1/2"}>
+                            <div className={'flex justify-start items-end w-full flex-col'}>
                                 <div className='absolute z-10 rounded-full mr-4'>
                                     <Image
                                         src={'/circle.svg'}
@@ -138,12 +138,12 @@ export default function WorkingProcess() : JSX.Element {
                                         alt={'deployment'}
                                     />
                                 </div>
+                                <p className={styles.workingContentImageDescription}>
+                                    The final stage to making sure all code is merged in one place and continuously ready to
+                                    be shipped. Seamlessly taking the product from continuous delivery and deploying to
+                                    servers through.
+                                </p>
                             </div>
-                            <p className={styles.workingContentImageDescription}>
-                                The final stage to making sure all code is merged in one place and continuously ready to
-                                be shipped. Seamlessly taking the product from continuous delivery and deploying to
-                                servers through.
-                            </p>
                         </div>
                     </div>
                 </div>
