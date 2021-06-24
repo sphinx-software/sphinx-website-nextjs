37d77948e23c27689778be3c1518a6080e13f651
linhtran <linh.cntt097@gmail.com>
2021-06-24 11:14:08 +0700

    transition for menu mobile

--- layout/MenuMobile.tsx
+++ layout/MenuMobile.tsx
@@ -1,15 +1,17 @@
 import { FunctionComponent } from 'react'
 import styles from '../components/Home/Home.module.css'
+import classNames from 'classnames'
 
 type MenuMobileProps = {
   isShow: boolean
 }
 const MenuMobile: FunctionComponent<MenuMobileProps> = ({ isShow }) => {
+  const navBarStyles = isShow ? 'max-h-450' : 'max-h-0'
   return (
     <div
-      className={`${isShow ? 'md:hidden h-screen pt-11 bg-white' : 'hidden'}`}
+      className={`${navBarStyles} md:hidden pt-11 overflow-hidden transition-all duration-1000 ease-in-out`}
     >
-      <div className='flex flex-col justify-center px-2 pt-2 pb-3 space-y-1 sm:px-3 leading-11'>
+      <div className='flex flex-col justify-center px-2 pt-2 pb-2 space-y-1 sm:px-3 leading-11'>
         <a
           href='#'
           className='hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-center font-ABeeZee'
@@ -36,7 +38,7 @@ const MenuMobile: FunctionComponent<MenuMobileProps> = ({ isShow }) => {
         </a>
         <div className='flex items-center justify-center font-ABeeZee'>
           <button className={styles.ButtonJoinUs}>
-            <span className={styles.ButtonJoinUsText}>Join Us</span>
+            <p className={styles.ButtonJoinUsText}>Join Us</p>
           </button>
         </div>
       </div>
