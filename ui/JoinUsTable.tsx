782900da3a743e1c92080dcf2c32442b08a0d429
longlh1997 <longlh@sphinx-software.com>
2021-06-29 11:29:43 +0700

    Fix size responsive table

--- ui/JoinUsTable.tsx
+++ ui/JoinUsTable.tsx
@@ -55,10 +55,10 @@ declare type JoinTableProps = {
 
 const JoinTable: FunctionComponent<JoinTableProps> = ({ data }) => {
   return (
-    <div className={'joinTable pb-44 w-full flex justify-center'}>
+    <div className={'joinTable pb-44 w-full flex justify-center px-6 md:px-0 '}>
       <table
         className={
-          'overflow-hidden rounded-3xl w-2/3 bg-gray-850 bg-opacity-10'
+          'overflow-hidden rounded-3xl w-full md:w-2/3 bg-gray-850 bg-opacity-10'
         }
         style={{ boxShadow: '0px 0px 50px rgba(14, 28, 28, 0.05)' }}
       >
