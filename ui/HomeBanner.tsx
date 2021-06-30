b72f93288ea1e4742ff467b0030a7f4f7bc7e55a
linhtran <linh.cntt097@gmail.com>
2021-06-30 14:54:28 +0700

    animation for company overview

--- ui/HomeBanner.tsx
+++ ui/HomeBanner.tsx
@@ -1,6 +1,13 @@
 import Image from 'next/image'
-import imageBanner from '../public/imageBanner.svg'
 import styles from './../styles/Banner.module.css'
+import imageBannerCode from './../public/imageBannerCode.svg'
+import imageBannerCSS from './../public/imageBannerCSS.svg'
+import imageBannerPHP from './../public/imageBannerPHP.svg'
+import imageBannerLaptop from './../public/imageBannerLaptop.svg'
+import imageBannerSoftware from './../public/imageBannerSoftware.svg'
+import imageBannerXML from './../public/imageBannerXML.svg'
+import imageBannerSQL from './../public/imageBannerSQL.svg'
+import ScrollAnimation from 'react-animate-on-scroll'
 
 const HomeBanner = () => {
   return (
@@ -10,22 +17,64 @@ const HomeBanner = () => {
         backgroundImage: "url('/bgWConnerUp.svg')"
       }}
     >
-      <div className='mx-auto max-w-7xl mt-4 md:mt-10 pb-20 px-2 xl:pb-32 md:px-0'>
+      <div className='mx-auto max-w-7xl mt-4 py-8 pb-20 md:pb-28 md:pt-14 px-2 xl:pt-32 xl:pb-72 lg:px-0'>
         <div className='flex flex-col items-center md:flex-row'>
           <div className='md:w-1/2'>
-            <h1 className='text-40 leading-64 md:text-64 md:leading-80 text-white'>
-              CREATING THE INNOVATIVE FUTURE
-            </h1>
-            <p className='text-16 leading-32 text-gray-350'>
-              Providing the most professional technology services for clients
-              worldwide
-            </p>
+            <ScrollAnimation animateIn='animate__fadeInUp'>
+              <h1 className='text-40 leading-64 md:text-64 md:leading-80 text-white'>
+                CREATING THE INNOVATIVE FUTURE
+              </h1>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn='animate__fadeInUp'>
+              <p className='text-16 leading-32 text-gray-350'>
+                Providing the most professional technology services for clients
+                worldwide
+              </p>
+            </ScrollAnimation>
+
             <button className={styles.buttonContactUs}>
               <p>Contact Us</p>
             </button>
           </div>
-          <div className='mt-12'>
-            <Image src={imageBanner} alt='Sphinx Software' />
+          <div className='w-full mt-80 md:mt-24 md:w-1/2'>
+            <div className='w-full relative -top-60'>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__lightSpeedInRight'>
+                  <Image src={imageBannerPHP} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__lightSpeedInRight'>
+                  <Image src={imageBannerSQL} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__lightSpeedInRight'>
+                  <Image src={imageBannerCSS} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__lightSpeedInLeft'>
+                  <Image src={imageBannerLaptop} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__fadeInDown'>
+                  <Image src={imageBannerCode} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__fadeInUp'>
+                  <Image src={imageBannerSoftware} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+              <div className='absolute'>
+                <ScrollAnimation animateIn='animate__lightSpeedInRight'>
+                  <Image src={imageBannerXML} alt='Sphinx Software' />
+                </ScrollAnimation>
+              </div>
+            </div>
           </div>
         </div>
       </div>
