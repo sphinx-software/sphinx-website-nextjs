015e7bb4b0d7620e553c31b2bb32ddfadb0b0d5d
ducviet1696 <phamviet008@gmail.com>
2021-07-06 10:39:06 +0700

    Responsive WorkingProcess section

--- components/Home/WorkingProcess.tsx
+++ components/Home/WorkingProcess.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import React from 'react'
 import RequirementLine from '../../public/lineStep1.svg'
+import RequirementLineM from '../../public/lineStep1-mobile.svg'
 import DesignLine from '../../public/lineStep2.svg'
+import DesignLineM from '../../public/lineStep2-mobile.svg'
 import DevelopmentLine from '../../public/lineStep3.svg'
+import DevelopmentLineM from '../../public/lineStep3-mobile.svg'
 import DeploymentLine from '../../public/lineStep4.svg'
+import DeploymentLineM from '../../public/lineStep4-mobile.svg'
 import Circle from '../../public/circle.svg'
 import Analysis from '../../public/analysis.svg'
 import Design from '../../public/design.svg'
@@ -24,22 +28,25 @@ export default function WorkingProcess(): JSX.Element {
         </div>
         <div className='pt-24'>
           <div className='flex flex-col md:flex-row'>
-            <div className='flex-1 flex flex-col justify-center items-end border-l-2 md:border-l-0 md:border-r-2 border-solid border-gray-233031 border-opacity-20'>
+            <div className='flex-1 flex flex-col justify-center items-start md:items-end border-l-2 md:border-l-0 md:border-r-2 border-solid border-gray-233031'>
               <div className='pl-4 md:pl-0 pr-0 md:pr-10 bg-clip-text text-transparent bg-gradient-to-r from-yellow-FFC838 to-orange-E05529 bg-yellow-FFC838 bg-cover text-36 -mt-6'>
                 Requirement&apos;s analysis
               </div>
-              <div className='relative -scaleX-1 md:transform-none mt-10'>
-                <div className='absolute -right-3 -top-32 lg:-top-20'>
-                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-opacity-20 border-gray-233031' />
+              <div className='relative mt-10'>
+                <div className='absolute -left-3 md:left-auto md:-right-3 -top-32 lg:-top-20'>
+                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-gray-233031' />
                 </div>
-                <div style={{ marginRight: -2 }}>
+                <div className='hidden md:block'>
                   <Image src={RequirementLine} alt={'RequirementLine'} />
+                </div>
+                <div className='md:hidden'>
+                  <Image src={RequirementLineM} alt={'RequirementLine'} />
                 </div>
               </div>
             </div>
-            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-solid border-gray-233031 border-opacity-20 pl-3 md:pl-0'>
+            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-solid border-gray-233031 pl-3 md:pl-0'>
               <div className='relative'>
-                <div className='absolute -left-24 -bottom-20'>
+                <div className='absolute md:-left-24 md:-bottom-20'>
                   <Image src={Circle} alt={'circle'} />
                 </div>
                 <div>
@@ -54,9 +61,9 @@ export default function WorkingProcess(): JSX.Element {
             </div>
           </div>
           <div className='flex flex-col-reverse md:flex-row'>
-            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-r-0 md:border-r-2 border-solid border-gray-233031 border-opacity-20 pl-3 md:pl-0'>
+            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-r-0 md:border-r-2 border-solid border-gray-233031 pl-3 md:pl-0'>
               <div className='relative'>
-                <div className='absolute z-20'>
+                <div className='absolute md:-right-24 md:-bottom-20'>
                   <Image src={Circle} alt={'circle'} />
                 </div>
                 <div>
@@ -69,7 +76,7 @@ export default function WorkingProcess(): JSX.Element {
                 specifications goals and improves the product in the future.
               </div>
             </div>
-            <div className='flex-1 flex flex-col justify-center items-start border-l-2 md:border-l-0 border-solid border-gray-233031 border-opacity-20'>
+            <div className='flex-1 flex flex-col justify-center items-start border-l-2 md:border-l-0 border-solid border-gray-233031'>
               <div
                 className='pl-4 md:pl-10 bg-clip-text text-transparent bg-gradient-to-r from-yellow-FFC838 to-orange-E05529 bg-yellow-FFC838 text-36 pt-8'
                 style={{ width: 'fit-content', blockSize: 'fit-content' }}
@@ -80,29 +87,35 @@ export default function WorkingProcess(): JSX.Element {
                 <div className='absolute -left-3 -top-20'>
                   <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-gray-233031' />
                 </div>
-                <div style={{ marginLeft: -2 }}>
+                <div className='hidden md:block'>
                   <Image src={DesignLine} alt={'DesignLine'} />
+                </div>
+                <div className='md:hidden'>
+                  <Image src={DesignLineM} alt={'DesignLine'} />
                 </div>
               </div>
             </div>
           </div>
           <div className='flex flex-col md:flex-row'>
-            <div className='flex-1 flex flex-col justify-center items-end border-l-2 md:border-l-0 border-r-0 md:border-r-2 border-solid border-gray-233031 border-opacity-20'>
+            <div className='flex-1 flex flex-col justify-center items-start md:items-end border-l-2 md:border-l-0 border-r-0 md:border-r-2 border-solid border-gray-233031'>
               <div className='pl-4 md:pl-0 pr-0 md:pr-10 bg-clip-text text-transparent bg-gradient-to-r from-yellow-FFC838 to-orange-E05529 bg-yellow-FFC838 bg-cover text-36 pt-10'>
                 Development and Testing
               </div>
-              <div className='relative -scaleX-1 md:transform-none mt-8'>
-                <div className='absolute -right-3 -top-24 md:-top-16'>
-                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-opacity-20 border-gray-233031' />
+              <div className='relative mt-8'>
+                <div className='absolute -left-3 md:left-auto md:-right-3 -top-24 md:-top-16'>
+                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-gray-233031' />
                 </div>
-                <div style={{ marginRight: -2 }}>
+                <div className='hidden md:block'>
                   <Image src={DevelopmentLine} alt={'DevelopmentLine'} />
+                </div>
+                <div className='md:hidden'>
+                  <Image src={DevelopmentLineM} alt={'DevelopmentLine'} />
                 </div>
               </div>
             </div>
-            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-solid border-gray-233031 border-opacity-20 pl-3 md:pl-0 pb-10'>
+            <div className='flex-1 flex flex-col justify-center items-center border-l-2 md:border-l-0 border-solid border-gray-233031 pl-3 md:pl-0 pb-10'>
               <div className='relative'>
-                <div className='absolute z-20'>
+                <div className='absolute z-20 md:-left-24 md:-bottom-20'>
                   <Image src={Circle} alt={'circle'} />
                 </div>
                 <div>
@@ -119,7 +132,7 @@ export default function WorkingProcess(): JSX.Element {
           <div className='flex flex-col-reverse md:flex-row'>
             <div className='flex-1 flex flex-col justify-center items-center pt-10'>
               <div className='relative'>
-                <div className='absolute z-20'>
+                <div className='absolute z-20 md:-right-24 md:-bottom-20'>
                   <Image src={Circle} alt={'circle'} />
                 </div>
                 <div>
@@ -140,11 +153,16 @@ export default function WorkingProcess(): JSX.Element {
               >
                 Deployment
               </div>
-              <div className='relative -mt-10'>
+              <div className='relative -mt-9 md:-mt-10'>
                 <div className='absolute -left-3 z-20'>
-                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-opacity-20 border-gray-233031' />
+                  <div className='w-25 h-25 rounded-full bg-gray-850 border-2 border-solid border-gray-233031' />
                 </div>
-                <Image src={DeploymentLine} alt={'DeploymentLine'} />
+                <div className='hidden md:block'>
+                  <Image src={DeploymentLine} alt={'DeploymentLine'} />
+                </div>
+                <div className='md:hidden'>
+                  <Image src={DeploymentLineM} alt={'DeploymentLine'} />
+                </div>
               </div>
             </div>
           </div>
