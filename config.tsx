649ba9d3003fef6c7c639beb4074b2f66ffbb584
ducviet1696 <phamviet008@gmail.com>
2021-07-06 10:59:10 +0700

    hidden service, blog page

--- config.tsx
+++ config.tsx
@@ -14,14 +14,14 @@ export const NAVS: NavConfig[] = [
     name: 'About SPHINX',
     href: '/about'
   },
-  {
-    name: 'Services',
-    href: '/services'
-  },
-  {
-    name: 'Blogs',
-    href: '/blogs'
-  },
+  // {
+  //   name: 'Services',
+  //   href: '/services'
+  // },
+  // {
+  //   name: 'Blogs',
+  //   href: '/blogs'
+  // },
   {
     name: 'Contact',
     href: '/contact'
