d1ce8dd5b95712c80de03c61d95c40baa93658fd
longlh1997 <longlh@sphinx-software.com>
2021-11-12 11:55:44 +0700

    add UI/UX Design job

--- config.tsx
+++ config.tsx
@@ -116,92 +116,99 @@ export const tableConfig = {
             'Phòng Hành chính Nhân sự: hr@sphinx-software.com',
             'Tel: 024 2266 3888 (Ms. Hà)',
             'Địa chỉ: Tầng 8, tòa Trường Thịnh, Tràng An Complex, số1 Phùng Chí Kiên, Nghĩa Đô, Cầu Giấy, Hà Nội.',
-            'FB: https://www.facebook.com/SphinxSoftware'
+            'FB: https://www.facebook.com/LifeatSphinxSoftware'
           ]
         }
       ]
     },
     {
-      id: 'se-php-nodejs',
-      position: 'Software Engineer (PHP/NodeJS)',
+      id: 'ui-ux-designer',
+      position: 'UI/UX Designer',
       time: 'Full Time',
       salary: 'Negotiation',
       description: [
         {
           title: 'MÔ TẢ CÔNG VIỆC',
           contents: [
-            'Tham gia phát triển các dự án về Web/Mobile với khách hàng quốc tế: Mỹ, Úc, Nhật.',
-            'Hỗ trợ xây dựng các kế hoạch kiểm thử, triển khai dự án.',
-            'Công việc khác sẽ trao đổi cụ thể khi phỏng vấn.'
+            'Tham gia trực tiếp vào quá trình phát triển sản phẩm, thiết kế UI/UX cho App Mobile, Website, System và các sản phẩm/dịch vụ khác theo yêu cầu;',
+            'Phối hợp với các bộ phận liên quan (QA, Development...) để lựa chọn phương án thiết kế phù hợp với dự án, sản phẩm/dịch vụ;',
+            'Hỗ trợ thực hiện các yêu cầu thiết kế của các bộ phận liên quan và các công việc khác theo sự phân công của quản lý trực tiếp.'
           ]
         },
         {
           title: 'YÊU CẦU CÔNG VIỆC',
           contents: [
-            'Sử dụng thành thạo ngôn ngữ/framework: PHP (Laravel) hoặc NodeJS(TypeScript).',
-            'Có kinh nghiệm phát triển dự án Web/Mobile.',
-            'Có kinh nghiệm làm việc với Docker.'
+            'Có ít nhất 1 năm kinh nghiệm làm việc ở vị trí tương đương;',
+            'Sử dụng thành thạo các công cụ thiết kế đồ họa thông dụng như: Figma, Sketch, Photoshop, Illustrator...;',
+            'Thành thạo vẽ wireframe, userflow;',
+            'Có khả năng tạo và trình diễn prototype thành thạo;',
+            'Có năng khiếu thẩm mỹ, tư duy sáng tạo tốt. Nắm vững các kiến thức cơ bản về đồ họa. Có khả năng cập nhật những xu hướng thiết kế mới;',
+            'Biết lắng nghe, đưa ra các gợi ý nhằm tổng hợp thông tin từ khách hàng, từ đó nắm bắt được yêu cầu sản phẩm, dịch vụ;',
+            'Có khả năng giao tiếp/đọc hiểu tiếng Anh;'
           ]
         },
         {
           title: 'ƯU TIÊN',
           contents: [
-            'Có hiểu biết về React và/hoặc NestJS.',
-            'Có kinh nghiệm làm việc với thị trường quốc tế.'
+            'Có kinh nghiệm làm việc với thị trường Nhật Bản;',
+            'Có hiểu biết về HTML, CSS;',
+            'Có thể làm việc trong môi trường áp lực;'
           ]
         },
         {
           title: 'YÊU CẦU KHÁC',
           contents: [
-            'Tiếng anh giao tiếp/đọc hiểu.',
-            'Có khả năng làm việc độc lập/teamwork.'
+            'Có khả năng làm việc độc lập & team-work;',
+            'Có tinh thần trách nhiệm cao trong công việc;'
           ]
         },
         {
           title: 'LƯƠNG & THƯỞNG',
           contents: [
-            'Mức lương NET: <b>từ 15 triệu</b> (thỏa thuận thêm tùy theo năng lực)',
+            'Lương NET - Mức lương khởi điểm hấp dẫn',
             'Thưởng lương tháng 13/năm + Thưởng dự án, thưởng Tết và các dịp Lễ...',
-            'Có lộ trình đào tạo và thăng tiến rõ ràng',
+            'Có lộ trình đào tạo và thăng tiến rõ ràng.',
             'Checkpoint 2 lần/năm.'
           ]
         },
         {
           title: 'CHẾ ĐỘ ĐÃI NGỘ & PHÚC LỢI',
           contents: [
-            'Nghỉ T7& CN + nghỉ phép theo quy định của pháp luật hiện hành',
+            'Nghỉ T7 & CN + nghỉ phép theo quy định của pháp luật hiện hành.',
             'Được tham gia BHXH, BHYT, BHTN theo quy định pháp luật hiện hành và quy định của công ty.',
             'Khám sức khỏe định kì 1 lần/năm.',
             'Du lịch định kỳ 1 lần/năm.',
-            'Phụ cấp ăn trưa, Gửi xe miễn phí, Hỗ trợ bữa chiều...',
-            'Được nhận quà tặng/tiền hỗ trợ vào các dịp: Sinh nhật, Nghỉ Lễ, sự kiện cá nhân khác...',
-            'Tham gia các hoạt động tập thể: Ăn nhậu, Kick Off, Đấu giải Game, Sinh nhật..'
+            'Phụ cấp ăn trưa; Gửi xe miễn phí; Hỗ trợ bữa chiều…',
+            'Được nhận quà tặng/các khoản hỗ trợ vào các dịp: Sinh nhật, NghỉLễ, sự kiện cá nhân khác…',
+            'Tham gia các hoạt động tập thể: Ăn nhậu, Kick Oﬀ, Đấu giải Game, Sinh nhật…'
           ]
         },
         {
           title: 'CƠ HỘI HỌC HỎI VÀ PHÁT TRIỂN',
           contents: [
-            'Trợ cấp tiếng Nhật và các chứng chỉ IT liên quan với nhiều hình thức hấp dẫn',
-            'Thường xuyên tổ chức các buổi đào nội nội bộ Team, Seminar để nâng cao kỹnăng và kiến thức',
-            'Máy PS4, Đàn Piano, Đàn Guitar, tủ sách, BoardGame…'
+            'Trợ cấp khóa học ngoại ngữ và các chứng chỉ IT liên quan với nhiều hình thức hấp dẫn;',
+            'Thường xuyên tổ chức các buổi đào nội nội bộ Team, Seminar để nâng cao kỹ năng và kiến thức.'
           ]
+        },
+        {
+          title: 'TIỆN ÍCH',
+          contents: ['Máy PS4, Đàn Piano, Đàn Guitar, tủ sách, BoardGame...']
         },
         {
           title: 'CÁCH THỨC ỨNG TUYỂN',
           contents: [
-            'Gửi CV đính kèm link GitHub hoặc các dự án đã từng thực hiện(nếu có)',
-            'Địa chỉ nhận CV: recruit.sphinx@gmail.com',
-            'Tiêu đề mail: [PHP/NODEJS] - Họ tên',
-            'Bộ phận tuyển dụng: recruit.sphinx@gmail.com'
+            'Gửi CV đính kèm portfolio nêu rõ quá trình công tác, kinh nghiệm làm việc...',
+            'Tiêu đề mail: <b>[UI/UX Designer] - Họ tên</b>',
+            'Địa chỉ nhận CV: <b>recruit.sphinx@gmail.com</b>'
           ]
         },
         {
           title: 'LIÊN HỆ HỖ TRỢ',
           contents: [
-            'Tel: 024.2266.3888 (Ms. An)',
+            'Tel: 024.2266.3888',
             'Địa chỉ: Tầng 8, tòa Trường Thịnh, Tràng An Complex, số1 Phùng Chí Kiên, Nghĩa Đô, Cầu Giấy, Hà Nội.',
             'Website: https://sphinx.vn/',
-            'FB: https://www.facebook.com/SphinxSoftware'
+            'FB: https://www.facebook.com/LifeatSphinxSoftware'
           ]
         }
       ]
@@ -215,23 +222,22 @@ export const tableConfig = {
         {
           title: 'MÔ TẢ CÔNG VIỆC',
           contents: [
-            'Phân tích yêu cầu người dùng. Phối hợp với khách hàng để triển khai và kiểm thử hệ thống phần mềm.',
-            'Lập trình web app theo thiết kế UI/UX.',
+            'Tham gia phát triển dự án Web/Mobile với đối tác Mỹ, Nhật...',
             'Phối hợp chặt chẽ với đội ngũ thiết kế để tìm kiếm cách tiếp cận thực tế nhất giúp phát triển UI mới. (Mọi chi tiết công việc sẽ trao đổi khi phỏng vấn)'
           ]
         },
         {
           title: 'YÊU CẦU CÔNG VIỆC',
           contents: [
-            'Lập trình UI/UX của ứng dụng web hiện đại, giàu tương tác với yêu cầu cao về chất lượng và độ tin cậy dựa trên ES6, ReactJS, Redux.',
-            'Có kinh nghiệm phát triển dự án Web/Mobile.',
-            'Đảm bảo tiến độ, chất lượng công việc và báo cáo thường xuyên tới quản lý. Tích cực cung cấp sản phẩm nhanh chóng tới khách hàng.'
+            'Tối thiểu 2 năm kinh nghiệm.',
+            'Có hiểu biết về ES6, ReactJS, Redux và API BackEnd.',
+            'Có khả năng làm việc độc lập/teamwork.'
           ]
         },
         {
           title: 'ƯU TIÊN',
           contents: [
-            'Có hiểu biết về ReactJS và/hoặc NodeJS.',
+            'Có kinh nghiệm sử dụng Docker.',
             'Có kinh nghiệm làm việc với thị trường quốc tế.'
           ]
         },
@@ -245,7 +251,7 @@ export const tableConfig = {
         {
           title: 'LƯƠNG & THƯỞNG',
           contents: [
-            'Mức lương NET: <b>từ 7,5 triệu</b> (thỏa thuận thêm tùy theo năng lực).',
+            'Mức lương NET: <b>~1000$</b> (thỏa thuận thêm tùy theo năng lực).',
             'Thưởng lương tháng 13/năm + Thưởng dự án, thưởng Tết và các dịp Lễ...',
             'Có lộ trình đào tạo và thăng tiến rõ ràng.',
             'Checkpoint 2 lần/năm.'
@@ -275,104 +281,17 @@ export const tableConfig = {
           title: 'CÁCH THỨC ỨNG TUYỂN',
           contents: [
             'Gửi CV đính kèm link GitHub hoặc các dự án đã từng thực hiện.(nếu có)',
-            'Tiêu đề mail: [ReactJS/NodeJS] - Họ tên',
-            'Địa chỉ nhận CV:recruit.sphinx@gmail.com'
-          ]
-        },
-        {
-          title: 'LIÊN HỆ HỖ TRỢ',
-          contents: [
-            'Tel: 024.2266.3888 (Ms. Hà)',
-            'Địa chỉ: Tầng 8, tòa Trường Thịnh, Tràng An Complex, số1 Phùng Chí Kiên, Nghĩa Đô, Cầu Giấy, Hà Nội.',
-            'Website: https://sphinx.vn/',
-            'FB: https://www.facebook.com/SphinxSoftware'
-          ]
-        }
-      ]
-    },
-    {
-      id: 'junior-nodejs-reactjs',
-      position: 'Junior - NodeJs/ReactJs',
-      time: 'Full Time',
-      salary: 'Up To 1000$',
-      description: [
-        {
-          title: 'SỐ LƯỢNG',
-          contents: ['02 Người.']
-        },
-        {
-          title: 'MÔ TẢ CÔNG VIỆC',
-          contents: [
-            'Tham gia phát triển dự án Web/Mobile với đối tác Mỹ, Nhật…',
-            'Phối hợp với khách hàng/đội nhóm để triển khai và kiểm thử hệ thống phần mềm. '
-          ]
-        },
-        {
-          title: 'YÊU CẦU CÔNG VIỆC',
-          contents: [
-            'Tối thiểu 2 năm kinh nghiệm ở vị trí tương đương.',
-            'Có hiểu biết về ES6, ReactJS, Redux và API BackEnd.',
-            'Có khả năng làm việc độc lập/teamwork.'
-          ]
-        },
-        {
-          title: 'ƯU TIÊN',
-          contents: [
-            'Có kinh nghiệm sử dụng Docker.',
-            'Có kinh nghiệm làm việc với thị trường quốc tế.'
-          ]
-        },
-        {
-          title: 'YÊU CẦU KHÁC',
-          contents: [
-            'Tiếng anh giao tiếp/đọc hiểu cơ bản.',
-            'Có khả năng chịu áp lực cao trong công việc.'
-          ]
-        },
-        {
-          title: 'LƯƠNG & THƯỞNG',
-          contents: [
-            'Mức lương NET: <b>up to 1000$</b> (thỏa thuận thêm tùy theo năng lực).',
-            'Thưởng lương tháng 13/năm + Thưởng dự án, thưởng Tết và các dịp Lễ…',
-            'Lộ trình thăng tiến rõ ràng.',
-            'Checkpoint 2 lần/năm.'
-          ]
-        },
-        {
-          title: 'CHẾ ĐỘ ĐÃI NGỘ & PHÚC LỢI',
-          contents: [
-            'Nghỉ T7& CN + nghỉ phép theo quy định của pháp luật hiện hành.',
-            'Được tham gia BHXH, BHYT, BHTN theo quy định pháp luật hiện hành và quy định của công ty.',
-            'Khám sức khỏe định kì 1 lần/năm.',
-            'Du lịch định kỳ 1 lần/năm.',
-            'Phụ cấp ăn trưa; Gửi xe miễn phí; Hỗ trợ bữa chiều…',
-            'Được nhận quà tặng/các khoản hỗ trợ vào các dịp: Sinh nhật, Nghỉ Lễ, sự kiện cá nhân khác…',
-            'Tham gia các hoạt động tập thể: Ăn nhậu, Kick Off, Đấu giải Game, Sinh nhật…'
-          ]
-        },
-        {
-          title: 'CƠ HỘI HỌC HỎI VÀ PHÁT TRIỂN',
-          contents: [
-            'Trợ cấp khóa học ngoại ngữ và các chứng chỉ IT liên quan với nhiều hình thức hấp dẫn;',
-            'Thường xuyên tổ chức các buổi đào nội nội bộ Team, Seminar để nâng cao kỹ năng và kiến thức.',
-            'Máy PS4, Đàn Piano, Đàn Guitar, tủ sách, BoardGame…'
-          ]
-        },
-        {
-          title: 'CÁCH THỨC ỨNG TUYỂN',
-          contents: [
-            'Gửi CV đính kèm link GitHub hoặc các dự án đã từng thực hiện (nếu có)',
-            'Tiêu đề mail: <b>[NodeJS/ReactJS] - Họ tên</b>',
+            'Tiêu đề mail: <b>[ReactJS/NodeJS] - Họ tên</b>',
             'Địa chỉ nhận CV: <b>recruit.sphinx@gmail.com</b>'
           ]
         },
         {
           title: 'LIÊN HỆ HỖ TRỢ',
           contents: [
-            'Tel: 024.2266.3888 ',
-            'Địa chỉ: Tầng 8, tòa Trường Thịnh, Tràng An Complex, số 1 Phùng Chí Kiên, Nghĩa Đô, Cầu Giấy, Hà Nội.',
+            'Tel: 024.2266.3888',
+            'Địa chỉ: Tầng 8, tòa Trường Thịnh, Tràng An Complex, số1 Phùng Chí Kiên, Nghĩa Đô, Cầu Giấy, Hà Nội.',
             'Website: https://sphinx.vn/',
-            'FB: https://www.facebook.com/SphinxSoftware'
+            'FB: https://www.facebook.com/LifeatSphinxSoftware'
           ]
         }
       ]
